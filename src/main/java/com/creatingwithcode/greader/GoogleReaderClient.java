f31de0e7a6f064da896bf7c44fa5e91007eda558
Robert McGuire <robert.m.mcguire@gmail.com>
2010-03-28 13:52:41 -0400

    GRC can now initialize with username/pass

--- src/main/java/com/creatingwithcode/greader/GoogleReaderClient.java
+++ src/main/java/com/creatingwithcode/greader/GoogleReaderClient.java
@@ -1,6 +1,7 @@
 package com.creatingwithcode.greader;
 
 import java.io.*;
+import java.util.*;
 import org.apache.commons.httpclient.*;
 import org.apache.commons.httpclient.methods.*;
 import org.apache.commons.httpclient.params.HttpMethodParams;
@@ -11,6 +12,12 @@ public class GoogleReaderClient {
   protected String username;
   protected String password;
   protected String sid;
+
+  protected HttpClient client;
+  protected ObjectMapper mapper;
+
+  protected int maxToFetch = 20;
+  protected long lastFetch = 0;
 
   public String getUsername() { return username; }
   public String getPassword() { return password; }
@@ -26,10 +33,27 @@ public class GoogleReaderClient {
   }
 
   public RecentItemsFeed getRecentItemsFeed() {
+    checkSID();
     String currTime = new Long(System.currentTimeMillis()/1000).toString();
-    String url = "http://www.google.com/reader/api/0/stream/contents/user/-/state/com.google/reading-list?ot=0&r=n&xt=user/-/state/com.google/read&n=20&ck=" + currTime + "&client=RssInvaders";
-    HttpClient client = new HttpClient();
-    GetMethod method = new GetMethod(url);
+    StringBuilder url = new StringBuilder("http://www.google.com/reader/api/0/stream/contents/user/-/state/com.google/reading-list");
+    Map<String,String> q_opts = new HashMap<String,String>();
+    q_opts.put("ot", new Long(lastFetch).toString());
+    q_opts.put("r","n");
+    q_opts.put("xt","user/-/state/com.google/read");
+    q_opts.put("n", new Long(maxToFetch).toString());
+    q_opts.put("ck", currTime);
+    q_opts.put("client","RssInvaders");
+    StringBuilder q = new StringBuilder();
+    String sep = "";
+    for(Map.Entry<String,String> entry : q_opts.entrySet()){
+      q.append(sep); q.append(entry.getKey()); q.append(entry.getValue());
+    }
+    if(q.length() > 0){
+      url.append("?"); url.append(q);
+    }
+
+    client = getHttpClient();
+    GetMethod method = new GetMethod(url.toString());
     method.setRequestHeader("Cookie", "SID=" + sid);
     InputStream respStream = null;
     RecentItemsFeed rif = null;
@@ -39,9 +63,7 @@ public class GoogleReaderClient {
         throw new RuntimeException("Failed to retrieve feed: " + method.getStatusLine());
       }
       respStream = method.getResponseBodyAsStream();
-      ObjectMapper mapper = new ObjectMapper();
-      mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES,
-                       false);
+      mapper = getObjectMapper();
       rif = mapper.readValue(respStream, RecentItemsFeed.class);
     } catch (Exception e) {
       throw new RuntimeException("Error retrieving feed.", e);
@@ -49,5 +71,52 @@ public class GoogleReaderClient {
       method.releaseConnection();
     }
     return rif;
+  }
+
+  protected HttpClient getHttpClient() {
+    if(client == null) {
+      client = new HttpClient();
+    }
+    return client;
+  }
+
+  protected ObjectMapper getObjectMapper() {
+    if(mapper == null) {
+      mapper = new ObjectMapper();
+      mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES,
+                       false);
+    }
+    return mapper;
+  }
+
+  protected void checkSID() {
+    if(sid == null) {
+      if((username == null) || (password == null)){
+        throw new RuntimeException("Cannot access Google Reader API without an SID or Username/Password combination.");
+      } else {
+        getSIDFromUsernameAndPass();
+      }
+    }
+  }
+
+  protected void getSIDFromUsernameAndPass(){
+    String url = "https://www.google.com/accounts/ClientLogin?service=reader&Email=" + username + "&Passwd=" + password;
+    client = getHttpClient();
+    GetMethod method = new GetMethod(url);
+    try {
+      int statusCode = client.executeMethod(method);
+      if (statusCode != HttpStatus.SC_OK) {
+        throw new RuntimeException("Failed to retrieve SID: " + method.getStatusLine());
+      }      
+      String resp = method.getResponseBodyAsString();
+      int indexSid = resp.indexOf("SID=") + 4;
+      int indexLsid = resp.indexOf("LSID=");
+      sid = resp.substring(indexSid, indexLsid);
+      username = null; password = null;
+    } catch (Exception e) {
+      throw new RuntimeException("Error retrieving SID.", e);
+    } finally {
+      method.releaseConnection();
+    }
   }
 }
--- src/test/java/GoogleReaderClientTest.java
+++ src/test/java/GoogleReaderClientTest.java
@@ -11,21 +11,11 @@ public class GoogleReaderClientTest extends TestCase {
   GoogleReaderClient grc = null;
 
   public void setUp() {
-    Map<String, String> env = System.getenv();
-    //g_user = env.get("GOOGLE_USERNAME");
-    //g_pass = env.get("GOOGLE_PASSWORD");
-    //if((g_user == null) || (g_pass == null)){
-    //  throw new RuntimeException("Please specify GOOGLE_USERNAME and GOOGLE_PASSWORD environment vars to run these tests.");
-    //}
-    //grc = new GoogleReaderClient(g_user, g_pass);
-    g_sid = env.get("GOOGLE_READER_SID");
-    if(g_sid == null){
-      throw new RuntimeException("Please specify GOOGLE_READER_SID environment var to run these tests.");
-    }
-    grc = new GoogleReaderClient(g_sid);
+    
   }
 
   public void testInitialization() {
+    grc = grc_from_env_sid();
     assertNotNull(grc);
     assertEquals(g_user, grc.getUsername());
     assertEquals(g_pass, grc.getPassword());
@@ -33,10 +23,41 @@ public class GoogleReaderClientTest extends TestCase {
   }
 
   public void testGetRecentItemsFeed() {
+    grc = grc_from_env_sid();
     RecentItemsFeed rif = grc.getRecentItemsFeed();
     assertNotNull(rif);
     assertNotNull(rif.getTitle());
     assertNotNull(rif.getItems());
     System.out.println("Title: " + rif.getTitle());
+  }
+
+  public void testLoginViaUserAndPassword() {
+    grc = grc_from_env_user_and_pass();
+    assertNull(grc.getSID());
+    RecentItemsFeed rif = grc.getRecentItemsFeed();
+    assertNotNull(rif);
+    assertNotNull(rif.getTitle());
+    assertNull(grc.getUsername());
+    assertNull(grc.getPassword());
+    assertNotNull(grc.getSID());
+  }
+
+  protected GoogleReaderClient grc_from_env_sid(){
+    Map<String, String> env = System.getenv();
+    g_sid = env.get("GOOGLE_READER_SID");
+    if(g_sid == null){
+      throw new RuntimeException("Please specify GOOGLE_READER_SID environment var to run this test.");
+    }
+    return (new GoogleReaderClient(g_sid));
+  }
+
+  protected GoogleReaderClient grc_from_env_user_and_pass() {
+    Map<String, String> env = System.getenv();
+    g_user = env.get("GOOGLE_READER_USER");
+    g_pass = env.get("GOOGLE_READER_PASS");
+    if((g_user == null) || (g_pass == null)) {
+      throw new RuntimeException("Please specify GOOGLE_READER_USER and GOOGLE_READER_PASS environment vars to run this test.");
+    }
+    return (new GoogleReaderClient(g_user, g_pass));
   }
 } 
